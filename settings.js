3b367a9bd93bfd9672279a4fc9250192554eac6c
Angel perez <44530759-papotioyt@users.noreply.replit.com>
2025-07-04 01:34:52 +0000

    Initial commit

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Kami BOT-MD',
+  author: '‎',
+  botName: "Kami BOT-MD",
+  botOwner: 'AngelNova', // Your name
+  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;
